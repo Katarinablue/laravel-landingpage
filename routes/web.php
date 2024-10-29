2f608c0df251909e49ef7d5fa7e9e8fe35317d27
Katarinabule <helomas72@gmail.com>
2024-10-29 15:41:40 +0700

    Challenge-3

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DepanController;
 
 Route::get('/', function () {
     return 'hello world';
 });
 
 Route::get('/task', [TaskController::class, 'index']);
+Route::get('/depan', [DepanController::class, 'index']);
